1c8f3f3e08b6107e022766abe2ec948f53936c59
Kouta Asai <scene.4913@gmail.com>
2017-04-01 23:13:24 +0900

    Add exercise slices

---
+++ basics/exercise-slices.go
@@ -0,0 +1,19 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	img := make([][]uint8, dy)
+	for y := 0; y < dy; y++ {
+		img[y] = make([]uint8, dy)
+		for x := 0; x < dx; x++ {
+			img[y][x] = uint8(x ^ y)
+		}
+	}
+
+	return img
+}
+
+func main() {
+	pic.Show(Pic)
+}
